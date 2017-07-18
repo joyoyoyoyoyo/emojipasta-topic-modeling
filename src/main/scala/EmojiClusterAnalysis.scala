9fcff5e941b7fc81b8e691d61cb7f865c76c95ed
Angel Ortega <cs.angelortega@gmail.com>
2017-07-17 23:41:30 -0700

    Ran frequency count job

--- src/main/scala/EmojiClusterAnalysis.scala
+++ src/main/scala/EmojiClusterAnalysis.scala
@@ -1,25 +1,38 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.functions.unix_timestamp
+
+import java.time._
 
 object EmojiClusterAnalysis {
 
   def main(args: Array[String]): Unit = {
 
-    sparkInit("local", "EmojiClusterAnalysis", emojiClusterAnalysis)
+    sparkInit("local", "EmojiClusterAnalysis", emojiClusterAnalysis _)
 
   }
 
-  def emojiClusterAnalysis(): Unit = {
-    ???
+  def emojiClusterAnalysis(sc: SparkContext)(loadFile: String): Unit = {
+    val emojiFrequency = sc.textFile(loadFile)
+      .flatMap(line => line.split("\\s+")) // .flatMap(_.split("\\s+"))
+      .map(word => (word, 1)) //
+      .reduceByKey(_ + _) // .reduceByKey{ case (x, y) => x + y }
+
+
+    val today = ZonedDateTime.now(ZoneId.of("UTC")).toString
+
+    emojiFrequency.saveAsTextFile(s"output/{$today}EmojiFrequency.txt")
+    
   }
 
 
   private def sparkInit(clusterUrl: String = "local", name: String,
-    callback: () => Unit) = {
+    callback: org.apache.spark.SparkContext => (String => Unit)) = {
       // creates a standalone application that runs on one thread on the local machine
       val conf = new SparkConf().setMaster("local").setAppName(name)
       val sc = new SparkContext(config = conf)
 
-      callback()
+      // callback(sc)(Thread.currentThread().getContextClassLoader().getResource("/EmojiPasta/emojipastacorpus.txt").toString)
+      callback(sc)(getClass.getResource("/EmojiPasta/emojipastacorpus.txt").toString)
 
       // must stop SparkContext or SparkSession, as of version >= 2.0.0
       sc.stop()
