ec6e7dccd6524b815f1a5bf3093af6c2521beb40
Angel Ortega <cs.angelortega@gmail.com>
2017-07-17 19:52:54 -0700

    Sparkstandalone is running on localmachine with one thread

--- src/main/scala/EmojiClusterAnalysis.scala
+++ src/main/scala/EmojiClusterAnalysis.scala
@@ -3,7 +3,9 @@ import org.apache.spark.{SparkConf, SparkContext}
 object EmojiClusterAnalysis {
 
   def main(args: Array[String]): Unit = {
+
+    // creates a standalone application that runs on one thread on the local machine
     val conf = new SparkConf().setMaster("local").setAppName("EmojiClusterAnalysis")
-    val sc = new SparkContext(conf)
+    val sc = new SparkContext(config = conf)
   }
 }
