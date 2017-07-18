0771e6082dd6b5e6d117e74b8764cc620d69095c
Angel Ortega <cs.angelortega@gmail.com>
2017-07-17 21:09:39 -0700

    callback func for spark run

--- src/main/scala/EmojiClusterAnalysis.scala
+++ src/main/scala/EmojiClusterAnalysis.scala
@@ -4,8 +4,24 @@ object EmojiClusterAnalysis {
 
   def main(args: Array[String]): Unit = {
 
-    // creates a standalone application that runs on one thread on the local machine
-    val conf = new SparkConf().setMaster("local").setAppName("EmojiClusterAnalysis")
-    val sc = new SparkContext(config = conf)
+    sparkInit("local", "EmojiClusterAnalysis", emojiClusterAnalysis)
+
+  }
+
+  def emojiClusterAnalysis(): Unit = {
+    ???
+  }
+
+
+  private def sparkInit(clusterUrl: String = "local", name: String,
+    callback: () => Unit) = {
+      // creates a standalone application that runs on one thread on the local machine
+      val conf = new SparkConf().setMaster("local").setAppName(name)
+      val sc = new SparkContext(config = conf)
+
+      callback()
+
+      // must stop SparkContext or SparkSession, as of version >= 2.0.0
+      sc.stop()
   }
 }
