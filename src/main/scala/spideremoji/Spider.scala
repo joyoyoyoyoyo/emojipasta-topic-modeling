5dae4e283e6849f24c94e969c2b2d98364ffff72
Angel Ortega <cs.angelortega@gmail.com>
2017-07-19 22:02:05 -0700

    failed spider 🕷️

--- src/main/scala/spideremoji/Spider.scala
+++ src/main/scala/spideremoji/Spider.scala
@@ -1,8 +1,16 @@
 package spideremoji
 
+import io.reactors.Reactor
+
 /**
   * Created by marblecake on 7/19/17.
   */
 object Spider {
-  
+
+  def main(args: Array[String]) = {
+    val url = "http://www.reddit.com/r/emojipasta/"
+    val response = scala.io.Source.fromURL(url)
+    System.out.println(response)
+  }
+
 }
