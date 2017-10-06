4d4a5120776f1886c1a3608cb4eeb2bcecf62fb5
Angel Ortega <an22ortega@gmail.com>
2017-10-06 00:26:11 -0700

    making the ball move

--- frontend-ember/app/components/galaga-canvas.js
+++ frontend-ember/app/components/galaga-canvas.js
@@ -9,14 +9,15 @@ export default Ember.Component.extend({
     // 2D-Sphere
     var x = canvas.width / 2;
     var y = canvas.height - 30;
+    var dx = 2;
+    var dy = -2;
 
     var draw = function() {
       ctx.beginPath();
-      ctx.arc(x, y, 10, 0, Math.PI * 2);
+      ctx.arc(x = x + dx, y = y + dy, 10, 0, Math.PI * 2);
       ctx.fillStyle = '#0095DD';
       ctx.fill();
       ctx.closePath();
-
     }
     // the draw function will be executed every 10 seconds
     setInterval(draw, 10);
