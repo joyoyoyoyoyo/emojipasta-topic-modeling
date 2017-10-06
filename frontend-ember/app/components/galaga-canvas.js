87f7846925dc8ddc24b7d73ae4222f376c4e9f2f
Angel Ortega <an22ortega@gmail.com>
2017-10-05 22:39:33 -0700

    successful render :)

--- frontend-ember/app/components/galaga-canvas.js
+++ frontend-ember/app/components/galaga-canvas.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    draw(some) {
-      alert(some)
+  didInsertElement() {
+    this._super(...arguments);
       // storing a reference to the canvas and creating a 2D rendering context
-      var canvas = document.getElementById("fight-to-the-death");
-      var ctx = canvas.getContext("2d");
+      var canvas =  document.getElementById('fight-to-the-death');
+      var ctx = canvas.getContext('2d');
 
       ctx.beginPath();
       ctx.rect(20, 40, 50, 50);
-      ctx.fillStyle = "#FF000";
+      ctx.fillStyle = '#FF0000';
+      ctx.fill();
       ctx.closePath();
-    }
+
   }
 });
