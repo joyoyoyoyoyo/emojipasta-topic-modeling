09f8f6e6cfa0555dad0be9248f36c843fe246902
Angel Ortega <an22ortega@gmail.com>
2017-10-05 23:16:27 -0700

    javascript string sty the readability way

--- frontend-ember/app/components/galaga-canvas.js
+++ frontend-ember/app/components/galaga-canvas.js
@@ -5,27 +5,27 @@ export default Ember.Component.extend({
     this._super(...arguments);
       // storing a reference to the canvas and creating a 2D rendering context
 
-      var canvas =  document.getElementById("fight-to-the-death");
-      var ctx = canvas.getContext("2d");
+      var canvas =  document.getElementById('fight-to-the-death');
+      var ctx = canvas.getContext('2d');
 
       // Red square
       ctx.beginPath();
       ctx.rect(20, 40, 50, 50);
-      ctx.fillStyle = "#FF0000";
+      ctx.fillStyle = '#FF0000';
       ctx.fill();
       ctx.closePath();
 
       // Green circle
       ctx.beginPath();
       ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-      ctx.fillStyle = "green";
+      ctx.fillStyle = 'green';
       ctx.fill();
       ctx.closePath();
 
       // Blue outline
       ctx.beginPath();
       ctx.rect(160, 10, 100, 40);
-      ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+      ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
       ctx.stroke();
       ctx.closePath();
   }
