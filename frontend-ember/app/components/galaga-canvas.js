2b77cec1bbf07a5e6ee8a9c0947590c5452e3943
Angel Ortega <an22ortega@gmail.com>
2017-10-06 00:35:25 -0700

    refactor ball render code

--- frontend-ember/app/components/galaga-canvas.js
+++ frontend-ember/app/components/galaga-canvas.js
@@ -3,23 +3,31 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement() {
     this._super(...arguments);
-    var canvas = document.getElementById('fight-to-the-death')
-    var ctx = canvas.getContext('2d')
+    var canvas = document.getElementById('fight-to-the-death');
+    var ctx = canvas.getContext('2d');
+
 
     // 2D-Sphere
-    var x = canvas.width / 2;
-    var y = canvas.height - 30;
-    var dx = 2;
-    var dy = -2;
+    var ball = {
+      x: canvas.width / 2,
+      y: canvas.height - 30,
+      dx: 2,
+      dy: -2
+    }
 
     var draw = function() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawBall(ball);
+    };
+
+    var drawBall = function(ball) {
       ctx.beginPath();
-      ctx.arc(x = x + dx, y = y + dy, 10, 0, Math.PI * 2);
+      ctx.arc(ball.x = ball.x + ball.dx, ball.y = ball.y + ball.dy, 10, 0, Math.PI * 2);
       ctx.fillStyle = '#0095DD';
       ctx.fill();
       ctx.closePath();
-    }
+    };
+
     // the draw function will be executed every 10 seconds
     setInterval(draw, 10);
   },
