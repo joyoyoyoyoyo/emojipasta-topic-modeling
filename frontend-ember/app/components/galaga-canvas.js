800e460fff99bfe33831c0bcba16085d4c632acf
Angel Ortega <an22ortega@gmail.com>
2017-10-05 22:49:30 -0700

    comments and make the canvas larger

--- frontend-ember/app/components/galaga-canvas.js
+++ frontend-ember/app/components/galaga-canvas.js
@@ -7,11 +7,32 @@ export default Ember.Component.extend({
       var canvas =  document.getElementById('fight-to-the-death');
       var ctx = canvas.getContext('2d');
 
+      // Red square
       ctx.beginPath();
       ctx.rect(20, 40, 50, 50);
       ctx.fillStyle = '#FF0000';
       ctx.fill();
       ctx.closePath();
 
+      // Green circle
+      ctx.beginPath();
+      ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+      ctx.fillStyle = "green";
+      ctx.fill();
+      ctx.closePath();
   }
+
 });
+
+
+// TODO:Add UI menu:
+/**
+  * IM FORGETTING YOU, GUMWAA!😭👋
+  * ██]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] 10% complete.....
+  * ████]]]]]]]]]]]]]]]]]]]]]]]]]]] 35% complete....
+  * ███████]]]]]]]]]]]]]]]] 60% complete....
+  * ███████████] 99% complete..... 🚫ERROR!🚫
+  **/
+// Font stuff for later
+// ctx.font = '48px serif';
+// ctx.fillText('😭', 50, 100);
