a154d56c9068b2d4448757135666a3b8be2c9aa4
Angel Ortega <an22ortega@gmail.com>
2017-10-05 23:15:32 -0700

    added 3 shapes wooo

--- frontend-ember/app/components/galaga-canvas.js
+++ frontend-ember/app/components/galaga-canvas.js
@@ -4,13 +4,14 @@ export default Ember.Component.extend({
   didInsertElement() {
     this._super(...arguments);
       // storing a reference to the canvas and creating a 2D rendering context
-      var canvas =  document.getElementById('fight-to-the-death');
-      var ctx = canvas.getContext('2d');
+
+      var canvas =  document.getElementById("fight-to-the-death");
+      var ctx = canvas.getContext("2d");
 
       // Red square
       ctx.beginPath();
       ctx.rect(20, 40, 50, 50);
-      ctx.fillStyle = '#FF0000';
+      ctx.fillStyle = "#FF0000";
       ctx.fill();
       ctx.closePath();
 
@@ -19,6 +20,13 @@ export default Ember.Component.extend({
       ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
       ctx.fillStyle = "green";
       ctx.fill();
+      ctx.closePath();
+
+      // Blue outline
+      ctx.beginPath();
+      ctx.rect(160, 10, 100, 40);
+      ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+      ctx.stroke();
       ctx.closePath();
   }
 
